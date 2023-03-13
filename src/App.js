11e7d4ea61021661b59e2072ed1fa228967a6fd2
Ravi Gunasena <ravigunz@gmail.com>
2023-03-14 00:18:53 +0530

    add map

--- src/App.js
+++ src/App.js
@@ -2,18 +2,49 @@ import logo from './logo.svg';
 import './App.css';
 import MyPara from './components/MyPara';
 
+const courseContent = [
+  {
+  status:'completed', 
+  text: 'Basic HTML and CSS',
+},
+  {
+  status:'completed', 
+  text: 'Smell the React',
+},
+  {
+  status:'completed', 
+  text: 'JSX',
+},
+  {
+  status:'completed', 
+  text: 'ES6/ ES7',
+},
+  {
+  status:'completed', 
+  text: 'Babel',
+},
+  {
+  status:'on-going', 
+  text: 'Webpack',
+},
+  {
+  status:'not-started', 
+  text: 'React file system',
+},
+  {
+  status:'not-started', 
+  text: 'package.js',
+},
+];
+
 function App() {
   return (
     <div className="App">
       <h1 style={{color: 'red'}}>Hi from React</h1>
-      <MyPara status='completed' text={'Basic HTML and CSS'} />
-      <MyPara status='completed' text={'Smell the React'} />
-      <MyPara status='completed' text={'JSX'} />
-      <MyPara status='completed' text={'ES6/ ES7'} />
-      <MyPara status='on-going' text={'Babel'} />
-      <MyPara status='not-started' text={'Webpack'} />
-      <MyPara status='not-started' text={'React file system'} />
-      <MyPara status='not-started' text={'package.js'} />
+      {courseContent.map((val, key) => {
+        return <MyPara key={key} status={val.status} text={val.text} />
+      })}
+      
     </div>
   );
 }
